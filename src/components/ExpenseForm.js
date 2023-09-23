e6e664d899cf43ea53de531bba95a8280f080dfc
DevSua <tndk5321@naver.com>
2023-09-24 02:34:45 +0900

    초기 세팅 및 디자인

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import "./ExpenseForm.css"
+
+export class ExpenseForm extends Component {
+  render() {
+    return (
+      <form>
+        <div className='form-center'>
+            <div className='form-group'>
+              <label htmlFor='charge'>지출 항목</label>
+                <input 
+                className='form-control'
+                type='text'
+                id='charge'
+                name='charge'
+                placeholder='예) 렌트비'/>
+            </div>
+            <div className='form-group'>
+              <label htmlFor='amount'>비용</label>
+                <input 
+                className='form-control'
+                type='number'
+                id='amount'
+                name='amount'
+                placeholder='예) 100원'/>
+            </div>
+
+        </div>
+        <button type='submit' className='btn'>
+            제출
+        </button>
+      </form>
+    )
+  }
+}
+
+export default ExpenseForm
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import ExpenseItem from './ExpenseItem'
+import './ExpenseList.css'
+
+export class ExpenseList extends Component {
+  render() {
+    return (
+      <>
+      <ul className='list'>
+        <ExpenseItem/>
+      </ul>
+      <button className='btn'>
+        목록 지우기
+      </button>
+
+      </>
+    )
+  }
+}
+
+export default ExpenseList
